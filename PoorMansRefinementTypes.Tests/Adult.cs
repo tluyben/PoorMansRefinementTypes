656039ee7e38b93632cfbf90b3f4732cc03dad2d
Tycho Luyben <tycho@e-lab.nl>
2020-04-05 17:45:03 +0200

    more complete tests

--- PoorMansRefinementTypes.Tests/Adult.cs
+++ PoorMansRefinementTypes.Tests/Adult.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using PoorMansRefinementTypes.Lib;
+
 namespace PoorMansRefinementTypes.Tests
 {
     public class Adult : Person
     {
+        public static int ErrorAge(int a)
+        {
+            return -1;
+        }
+
+        public static bool IsOfAdultAge(int a)
+        {
+            return a < 18;
+        }
+
+        [Ensures(ValidationMethod = nameof(IsOfAdultAge), Throw = false, GetDefault = nameof(ErrorAge))]
         public override int Age { get; set; }
 
     }
